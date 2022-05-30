0a216fb278eb6b2f7432f731f0adf29561e5c890
Regitze-Adrian <regi5541@easv365.dk>
2022-05-30 18:00:33 +0200

    Fonts added + start on About us
    fonts Roboto and Roboto Slab added, and start on subpage 'About us'

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,9 @@
 <template>
+
+<div class="bannerbox"><img src="../assets/aboutus.jpg" alt="about us"> 
+<div class="bannertext">About us</div>
+</div>
+
   <div>
     <p>
       This is an about site
@@ -11,5 +16,28 @@
 </script>
 
 <style lang="scss" scoped>
+
+img {
+  height: 200px;
+  width: 100%;
+  object-fit: cover;
+  object-position: 0 70%;
+  
+}
+
+.bannerbox {
+  text-align: center;
+  position: relative;
+}
+
+.bannertext {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 48px;
+  color: white;
+  font-family: 'Roboto Slab', serif;
+}
 
 </style>
