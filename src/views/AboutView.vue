e1154a90d01b3f4fa3a94b72a38a9fed2dae2030
Regitze-Adrian <regi5541@easv365.dk>
2022-05-31 17:10:08 +0200

    Folders added in assets
    Folders added in assets

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -18,10 +18,10 @@
 </div>
 
 <div class="bubblecontainer">
-  <div class="bluebubble"> <img src="../assets/icon-microphone.png" alt="microphone icon"> <p> To facilitate events for tech startups and offer access to investors and students in the southern region.</p></div>
-  <div class="bluebubble"> <img src="../assets/icon-raisedfists.png" alt="raised fists icon"> <p> To strengthen the startup and tech eco system in the southern region through a powerful community.</p></div> 
-  <div class="bluebubble"> <img src="../assets/icon-chat.png" alt="chat icon"> <p> Match commercial founders with tech founders to create new tech ventures.</p></div>
-  <div class="bluebubble"> <img src="../assets/icon-personpin.png" alt="person pin icon"> <p> Inspire with inspirational talks from successful tech founders, industry leaders, and business angels.</p></div>
+  <div class="bluebubble"> <img src="../assets/icons-aboutus/icon-microphone.png" alt="microphone icon"> <p> To facilitate events for tech startups and offer access to investors and students in the southern region.</p></div>
+  <div class="bluebubble"> <img src="../assets/icons-aboutus/icon-raisedfists.png" alt="raised fists icon"> <p> To strengthen the startup and tech eco system in the southern region through a powerful community.</p></div> 
+  <div class="bluebubble"> <img src="../assets/icons-aboutus/icon-chat.png" alt="chat icon"> <p> Match commercial founders with tech founders to create new tech ventures.</p></div>
+  <div class="bluebubble"> <img src="../assets/icons-aboutus/icon-personpin.png" alt="person pin icon"> <p> Inspire with inspirational talks from successful tech founders, industry leaders, and business angels.</p></div>
 </div>
 
 <div class="blueseparationline"></div>
@@ -32,19 +32,19 @@
 
 <div class="bubblecontainer">
   <div class="bubbletitle"> <h3> Inspirational talks </h3>
-    <div class="orangebubble"> <img src="../assets/icon-buildtool.png" alt="build tool icon"> <p>  We partner with inspirational leaders and tech entrepreneurs who have either built own venture or invested in startups. They go on stage to hand out wisdom from the biggest success to various failures. This to tell the true story of building tech companies and to inspire others.</p>
+    <div class="orangebubble"> <img src="../assets/icons-aboutus/icon-buildtool.png" alt="build tool icon"> <p>  We partner with inspirational leaders and tech entrepreneurs who have either built own venture or invested in startups. They go on stage to hand out wisdom from the biggest success to various failures. This to tell the true story of building tech companies and to inspire others.</p>
     </div>
   </div>
   <div class="bubbletitle"> <h3> Investor pitches </h3>
-    <div class="orangebubble"> <img src="../assets/icon-moneychat.png" alt="money chat icon"> <p> We gather tech startups and smart investors to match the two so we can help fuel the next generation of tech startups in the region. Startups will be chosen from four parameters such as: Team, Product/service, Market potential and business model.</p>
+    <div class="orangebubble"> <img src="../assets/icons-aboutus/icon-moneychat.png" alt="money chat icon"> <p> We gather tech startups and smart investors to match the two so we can help fuel the next generation of tech startups in the region. Startups will be chosen from four parameters such as: Team, Product/service, Market potential and business model.</p>
       </div>
     </div> 
   <div class="bubbletitle"> <h3> Co-founder meetups </h3>
-    <div class="orangebubble"> <img src="../assets/icon-handshake.png" alt="handshake icon"> <p> It is often hard to find the right co-founder for your startup. We setup a match-making event where commercial founders together with tech development founders can setup the right team from the start.</p>
+    <div class="orangebubble"> <img src="../assets/icons-aboutus/icon-handshake.png" alt="handshake icon"> <p> It is often hard to find the right co-founder for your startup. We setup a match-making event where commercial founders together with tech development founders can setup the right team from the start.</p>
       </div>
     </div>
   <div class="bubbletitle"> <h3> Startups pitch for student jobs </h3>
-    <div class="orangebubble"> <img src="../assets/icon-arrows.png" alt="arrows icon"> <p> A student job at a startup can be super exciting. Therefore we gather curious students from relevant educations and put them in front of startups looking to strengthen their core team.</p>
+    <div class="orangebubble"> <img src="../assets/icons-aboutus/icon-arrows.png" alt="arrows icon"> <p> A student job at a startup can be super exciting. Therefore we gather curious students from relevant educations and put them in front of startups looking to strengthen their core team.</p>
       </div>
     </div>
 </div>
@@ -57,18 +57,18 @@
 
 <table>
   <tr>
-    <td width="400px"> <a href="https://innovationsfonden.dk/da"> <img src="../assets/Innovationsfonden-logo.png" alt="innovationsfonden logo"></a> </td>
-    <td width="400px"> <a href="https://www2.deloitte.com/dk/da.html"> <img src="../assets/Deloitte-logo.png" alt="deloitte logo"></a> </td>
+    <td width="400px"> <a href="https://innovationsfonden.dk/da"> <img src="../assets/partners/Innovationsfonden-logo.png" alt="innovationsfonden logo"></a> </td>
+    <td width="400px"> <a href="https://www2.deloitte.com/dk/da.html"> <img src="../assets/partners/Deloitte-logo.png" alt="deloitte logo"></a> </td>
   </tr>
   <tr>
-    <td width="400px"> <a href="https://www.easv.dk/da/"> <img src="../assets/EASV-logo.png" alt="easv logo"></a> </td>
-    <td width="400px"> <a href="https://www.businessesbjerg.com/da/"> <img src="../assets/BE-logo.png" alt="BE logo"></a> </td>
-    <td width="400px"> <a href="https://vf.dk/"> <img src="../assets/vaekstfonden-logo.png" alt="vaekstfonden logo"></a> </td>
+    <td width="400px"> <a href="https://www.easv.dk/da/"> <img src="../assets/partners/EASV-logo.png" alt="easv logo"></a> </td>
+    <td width="400px"> <a href="https://www.businessesbjerg.com/da/"> <img src="../assets/partners/BE-logo.png" alt="BE logo"></a> </td>
+    <td width="400px"> <a href="https://vf.dk/"> <img src="../assets/partners/vaekstfonden-logo.png" alt="vaekstfonden logo"></a> </td>
   </tr>
   <tr>
-    <td width="400px"> <a href="https://www.aau.dk/"> <img src="../assets/AAU-logo.png" alt="AAU logo"></a> </td>
-    <td width="400px"> <img src="../assets/A-Thomsen-Holding-logo.png" alt="A. Thomsen Holding logo"> </td>
-    <td width="400px"> <a href="https://www.ebanklauridsen.dk/"> <img src="../assets/E-Bank-Lauridsen-Holding-logo.png" alt="E. Bank Lauridsen Holding logo"></a> </td>
+    <td width="400px"> <a href="https://www.aau.dk/"> <img src="../assets/partners/AAU-logo.png" alt="AAU logo"></a> </td>
+    <td width="400px"> <img src="../assets/partners/A-Thomsen-Holding-logo.png" alt="A. Thomsen Holding logo"> </td>
+    <td width="400px"> <a href="https://www.ebanklauridsen.dk/"> <img src="../assets/partners/E-Bank-Lauridsen-Holding-logo.png" alt="E. Bank Lauridsen Holding logo"></a> </td>
   </tr>
 </table>
 
@@ -274,5 +274,6 @@ table tr td img {
     width: 50%;
     height: 50%;
 }
+
 
 </style>
